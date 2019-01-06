6da3241748cd3a982dd51a690047b3b61652b375
Robert McKenney <robert@mckenney.ca>
2019-01-06 13:49:11 -0500

    Starter files

---
+++ helloStudents.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// 1. Read the JSON file into a variable called students
+
+// 2. Iterate over the students array and print Hello with their full names to the console
+// e.g. Hello Walter Baker
+
+// 3. Print out the number of last names starting with the letter D
+// e.g. Count of last names starting with D is 1
